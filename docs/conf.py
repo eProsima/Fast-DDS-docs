f9d9dbf0334d5f83c02decec3bf4c2e0bfd3bdb5
Santiago Munoz <santiagomunoz@eprosima.com>
2016-11-23 08:51:10 +0100

    css changes (logo)

--- docs/conf.py
+++ docs/conf.py
@@ -155,6 +155,13 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+        'css_files': [
+            '_static/css/logo_css.css', #logo
+            ],
+        }
+
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.

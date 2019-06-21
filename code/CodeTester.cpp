741acb09b972be454a2904bcb745decafc12c284
Raquel Alvarez <raquelalvarez@eprosima.com>
2019-06-21 09:32:36 +0200

    Refs #5290 Fixing code tester

--- code/CodeTester.cpp
+++ code/CodeTester.cpp
@@ -647,10 +647,10 @@ subscriber_attr.qos.m_disablePositiveACKs.enabled = true;
 //PUBSUB_API_CONF_PUBSUB_LIVELINESS
 publisher_attr.qos.m_liveliness.announcement_period = 0.5;
 publisher_attr.qos.m_liveliness.lease_duration = 1;
-publisher_attr.qos.m_liveliness.kind = AUTOMATIC;
+publisher_attr.qos.m_liveliness.kind = AUTOMATIC_LIVELINESS_QOS;
 
 subscriber_attr.qos.m_liveliness.lease_duration = 1;
-subscriber_attr.qos.m_liveliness.kind = AUTOMATIC;
+subscriber_attr.qos.m_liveliness.kind = AUTOMATIC_LIVELINESS_QOS;
 //!--
 
 //PUBSUB_API_CONF_PUBSUB_RESOURCE_LIMITS

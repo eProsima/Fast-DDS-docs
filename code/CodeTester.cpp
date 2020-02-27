b07dca8c3d312805b681ab7a5acbd6b2422b1881
RaulSanchez <raul@eprosima.com>
2020-02-27 09:31:56 +0100

    Added working sections for the discovery protocol

--- code/CodeTester.cpp
+++ code/CodeTester.cpp
@@ -485,6 +485,18 @@ writer_attr.times.heartbeatPeriod.seconds = 0;
 writer_attr.times.heartbeatPeriod.nanosec = 500000000; //500 ms
 //!--
 
+//CONF-DISCOVERY-INTRO-EXAMPLE
+//!--
+
+//CONF-SIMPLE-DISCOVERY-EXAMPLE
+//!--
+
+//CONF-STATIC-DISCOVERY-EXAMPLE
+//!--
+
+//CONF-SERVER-DISCOVERY-EXAMPLE
+//!--
+
 }
 
 //API-DISCOVERY-TOPICS-LISTENER

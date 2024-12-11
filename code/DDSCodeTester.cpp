e8be4237079d2f143a623a51c4da18c18f27ddaf
Miguel Company <miguelcompany@eprosima.com>
2024-12-11 16:20:55 +0100

    Document new `transmit_algorithms_as_legacy` on builtin security plugins (#974)
    * Refs #19925. Add documentation of new property in PKIDH.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #19925. Add documentation of new property in Permissions.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    Co-authored-by: Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
    (cherry picked from commit cc95496dc6ee088366f00dec5c4213d4e3b2c86d)
    
    # Conflicts:
    #	code/DDSCodeTester.cpp
    #	code/XMLTester.xml
    #	docs/fastdds/property_policies/security.rst
    #	docs/fastdds/security/auth_plugin/auth_plugin.rst

--- code/DDSCodeTester.cpp
+++ code/DDSCodeTester.cpp
@@ -593,6 +593,15 @@ void dds_domain_examples()
         pqos.properties().properties().emplace_back(
             "dds.sec.auth.builtin.PKI-DH.password",
             "domainParticipantPassword");
+<<<<<<< HEAD
+=======
+        pqos.properties().properties().emplace_back(
+            "dds.sec.auth.builtin.PKI-DH.preferred_key_agreement",
+            "ECDH");
+        pqos.properties().properties().emplace_back(
+            "dds.sec.auth.builtin.PKI-DH.transmit_algorithms_as_legacy",
+            "true");
+>>>>>>> cc95496 (Document new `transmit_algorithms_as_legacy` on builtin security plugins (#974))
         //!--
     }
     {
@@ -628,6 +637,9 @@ void dds_domain_examples()
         pqos.properties().properties().emplace_back(
             "dds.sec.access.builtin.Access-Permissions.permissions",
             "file://certs/permissions.smime");
+        pqos.properties().properties().emplace_back(
+            "dds.sec.access.builtin.Access-Permissions.transmit_algorithms_as_legacy",
+            "true");
         //!--
     }
     {

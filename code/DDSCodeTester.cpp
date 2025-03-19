391c521639671c69546505afa892ad217e409df5
EugenioCollado <121509066+EugenioCollado@users.noreply.github.com>
2025-03-19 19:53:10 +0100

    Documentation for flow controller configuration for built-in endpoints (#1042)
    * Feature flow controller
    
    Signed-off-by: Eugenio Collado <eugeniocollado@eprosima.com>
    
    * Small change
    
    Signed-off-by: Eugenio Collado <eugeniocollado@eprosima.com>
    
    ---------
    
    Signed-off-by: Eugenio Collado <eugeniocollado@eprosima.com>

--- code/DDSCodeTester.cpp
+++ code/DDSCodeTester.cpp
@@ -4705,6 +4705,8 @@ void dds_qos_examples()
         participant_qos.wire_protocol().easy_mode("127.0.0.1");
         // Increase mutation tries
         participant_qos.wire_protocol().builtin.mutation_tries = 300u;
+        // Use a specific flow controller to control the builtin writers
+        participant_qos.wire_protocol().builtin.flow_controller_name = "AlreadyExistingFlowController";
         // Use modified QoS in the creation of the DomainParticipant entity
         participant_ = factory_->create_participant(domain, participant_qos);
         //!--
@@ -6698,6 +6700,9 @@ void dds_usecase_examples()
         // Register flow controller on participant
         DomainParticipantQos participant_qos;
         participant_qos.flow_controllers().push_back(flow_control_300k_per_sec);
+
+        // [OPTIONAL] Link builtin writers to any of the registered flow controllers
+        participant_qos.wire_protocol().builtin.flow_controller_name = "example_flow_controller";
 
         // .... create participant and publisher
 

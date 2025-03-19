38b6b7fdfed634be29c60f6e2a85216c827f218b
Miguel Company <miguelcompany@eprosima.com>
2025-03-19 13:52:10 +0100

    Fix conflicts
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- code/DDSCodeTester.cpp
+++ code/DDSCodeTester.cpp
@@ -2849,13 +2849,6 @@ void dds_dataWriter_examples()
                     return;
                 }
             }
-<<<<<<< HEAD
-
-            // The data instance can be reused to publish new values,
-            // but delete it at the end to avoid leaks
-            custom_type_support->deleteData(data);
-=======
->>>>>>> c440452 (Fix example of writer loans (#946))
             //!--
         }
 

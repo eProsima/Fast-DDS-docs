c66212f13bd67daebae97523574e329a5df622fb
Mario Dominguez <mariodominguez@eprosima.com>
2025-05-14 10:25:06 +0200

    Refs #23169: Add docs
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- code/DDSCodeTester.cpp
+++ code/DDSCodeTester.cpp
@@ -988,7 +988,7 @@ void dds_domain_examples()
 
         pqos.properties().properties().emplace_back(
             "fastdds.serialize_optional_qos",
-            "True");
+            "true"); // True or TRUE or 1
         //!--
     }
 }

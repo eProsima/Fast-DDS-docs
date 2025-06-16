3d31dd6ff233b30ba4ccaa2245b9b8d69388318c
Miguel Company <miguelcompany@eprosima.com>
2025-06-16 12:12:54 +0200

    Refs #23325. Regenerate code.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- code/Examples/C++/RpcClientServerFeed/src/types/calculatorClient.cxx
+++ code/Examples/C++/RpcClientServerFeed/src/types/calculatorClient.cxx
@@ -205,6 +205,7 @@ private:
             std::shared_ptr<T> result,
             std::promise<TResult>& promise)
     {
+        result->info.related_sample_identity.writer_guid(requester_->get_requester_reader()->guid());
         if (fdds::RETCODE_OK == requester_->send_request((void*)&request, result->info))
         {
             std::lock_guard<std::mutex> _ (mtx_);
@@ -224,6 +225,7 @@ private:
             const RequestType& request,
             std::shared_ptr<T> result)
     {
+        result->info.related_sample_identity.writer_guid(requester_->get_requester_reader()->guid());
         if (fdds::RETCODE_OK == requester_->send_request((void*)&request, result->info))
         {
             std::lock_guard<std::mutex> _ (mtx_);
@@ -662,7 +664,7 @@ private:
         }
 
         void set_exception(
-                std::exception_ptr exception)
+                std::exception_ptr exception) override
         {
             std::lock_guard<std::mutex> _(mtx_);
             if (!finished_)
@@ -1039,7 +1041,7 @@ private:
         }
 
         void set_exception(
-                std::exception_ptr exception)
+                std::exception_ptr exception) override
         {
             std::lock_guard<std::mutex> _(mtx_);
             if (!finished_)
@@ -1338,7 +1340,7 @@ private:
         }
 
         void set_exception(
-                std::exception_ptr exception)
+                std::exception_ptr exception) override
         {
             std::lock_guard<std::mutex> _(mtx_);
             if (!finished_)

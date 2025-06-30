aadf55d8d47865b5f7aff9a849c8251213a8d101
Miguel Company <miguelcompany@eprosima.com>
2025-06-30 17:20:23 +0200

    Refs #23365. Regenerate code.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- code/Examples/C++/RpcClientServerBasic/src/types/calculatorTypeObjectSupport.hpp
+++ code/Examples/C++/RpcClientServerBasic/src/types/calculatorTypeObjectSupport.hpp
@@ -38,6 +38,33 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
 
 namespace calculator_example {
+/**
+ * @brief Register Calculator_Request related TypeIdentifier.
+ *        Fully-descriptive TypeIdentifiers are directly registered.
+ *        Hash TypeIdentifiers require to fill the TypeObject information and hash it, consequently, the TypeObject is
+ *        indirectly registered as well.
+ *
+ * @param[out] type_ids TypeIdentifier of the registered type.
+ *             The returned TypeIdentifier corresponds to the complete TypeIdentifier in case of hashed TypeIdentifiers.
+ *             Invalid TypeIdentifier is returned in case of error.
+ */
+eProsima_user_DllExport void register_Calculator_Request_type_identifier(
+        eprosima::fastdds::dds::xtypes::TypeIdentifierPair& type_ids);
+
+/**
+ * @brief Register Calculator_Reply related TypeIdentifier.
+ *        Fully-descriptive TypeIdentifiers are directly registered.
+ *        Hash TypeIdentifiers require to fill the TypeObject information and hash it, consequently, the TypeObject is
+ *        indirectly registered as well.
+ *
+ * @param[out] type_ids TypeIdentifier of the registered type.
+ *             The returned TypeIdentifier corresponds to the complete TypeIdentifier in case of hashed TypeIdentifiers.
+ *             Invalid TypeIdentifier is returned in case of error.
+ */
+eProsima_user_DllExport void register_Calculator_Reply_type_identifier(
+        eprosima::fastdds::dds::xtypes::TypeIdentifierPair& type_ids);
+
+
 } // namespace calculator_example
 
 
